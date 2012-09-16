c80b0d563d30e808c0c6442c682d821083b54f2a
Jaffe <jaffe75@gmail.com>
2012-09-16 15:39:10 -0700

    changes for script

--- home.php
+++ home.php
@@ -36,12 +36,7 @@ get_header(); ?>
 			
 		</div><!-- content -->	
 		<div class="main-nav-menu">
-			<ul>
-				<li><a href="home">Home</a></li>
-				<li><a href="about">About</a></li>
-				<li><a href="networks">Networks</a></li>
-				<li><a href="contact">Contact</a></li>
-			</ul>
+
 		</div><!-- main nav menu -->
 	</div><!-- page wrap -->
 
